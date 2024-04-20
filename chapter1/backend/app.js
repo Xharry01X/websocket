0e3c442d5ed8760f2fcf6eaea50fa6f48d09f385
harshit-506 <supriyamohapatra658@gmail.com>
2024-04-20 11:35:22 +0530

    Real time data changes

--- chapter1/backend/app.js
+++ chapter1/backend/app.js
@@ -1,1 +1,24 @@
-const 
+const express=require("express")
+const http=require("http")
+const WebSocket=require("ws")
+
+const app=express()
+const server=http.createServer(app);
+const wss=new WebSocket.Server({server})
+
+
+const port=3100
+
+wss.on('connection',(ws)=>{
+ws.on('message',(message)=>{
+    console.log('received: %s',message);
+});
+setInterval(()=>{
+    const data=Math.random().toString()
+    ws.send(data)
+},1000)
+})
+
+server.listen(port,()=>{
+    console.log(`Server live at ${port}`);
+})
--- chapter1/frontend/src/App.js
+++ chapter1/frontend/src/App.js
@@ -1,11 +1,25 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import './App.css';
 
 function App() {
-  const [data,setData]=useState(0)
+  const [data,setData]=useState('')
+
+useEffect(()=>{
+const ws=new WebSocket('ws://localhost:3100')
+
+ws.onmessage=(e)=>{
+  setData(e.data)
+};
+return ()=>{
+  ws.close()
+}
+},[])
+
+
   return (
     <div className="App">
-    
+    <h1>Websocket Demo</h1>
+    <p>Data from websocket: {data}</p>
     </div>
   );
 }
