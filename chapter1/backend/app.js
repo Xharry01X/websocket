73488f510f3aec76bfc2bb86c41a1bd4b703cf82
harshit-506 <supriyamohapatra658@gmail.com>
2024-04-20 11:49:38 +0530

    will make chat functionality

--- chapter1/backend/app.js
+++ chapter1/backend/app.js
@@ -10,9 +10,10 @@ const wss=new WebSocket.Server({server})
 const port=3100
 
 wss.on('connection',(ws)=>{
-ws.on('message',(message)=>{
-    console.log('received: %s',message);
-});
+    //will use later for real tuime functionality
+// ws.on('message',(message)=>{
+//     console.log('received: %s',message);
+// });
 setInterval(()=>{
     const data=Math.random().toString()
     ws.send(data)
